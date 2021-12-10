e7f5da0ad124763d24aecbcdd734c7a3c140a91a
awaisshah228 <awaisshah228@gmail.com>
2021-12-10 13:03:41 +0500

    done

---
+++ src/AdminDashboardControls/CompanyOrder/CompanyOrder.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import DashboardBars from '../../components/DashboardBars/DashboardBars'
+import DashboardPagination from '../../components/AdminDashboard/DashboardPagination/DashboardPagination'
+import ListTable2 from '../../components/AdminDashboard/ListTable2/ListTable2'
+import goBack from '../../helpers/goBack'
+
+
+const CompanyOrder = () => {
+    const person=[
+        {
+            td1 : "1",
+            td2 : "3746",
+            td3 : "762",
+            td4:  "70",
+            td5 : "34937974",
+            td6: "active",
+
+        },
+        {
+            td1 : "1",
+            td2 : "3746",
+            td3 : "762",
+            td4:  "70",
+            td5 : "34937974",
+            td6: "active",
+
+        },
+        {
+            td1 : "1",
+            td2 : "3746",
+            td3 : "762",
+            td4:  "70",
+            td5 : "34937974",
+            td6: "active",
+
+        },
+       
+    ]
+    return (
+        <div>
+            <DashboardBars>
+            <section id="show-merchants">
+            {/* <h1 className="title mb-4">Customers</h1> */}
+            <h4 className="title mb-4">
+                    <a onClick={goBack}>Company A</a>
+                    <span>&#x3e; Orders</span>
+                </h4>
+            <ListTable2 cols={["Order ID","Transaction Date","Item", "Customer Name", "Amount","Status",]}  data={person} buttontxt={"View Invoice"}/>
+            
+            <DashboardPagination />
+        </section>
+            </DashboardBars>
+           
+          
+        </div>
+    )
+}
+
+export default CompanyOrder
---
+++ src/AdminDashboardControls/OrderInvoice/OrderInvoice.js
@@ -0,0 +1,111 @@
+import React from "react";
+import DashboardBars from "../../components/DashboardBars/DashboardBars";
+import DashboardPagination from "../../components/AdminDashboard/DashboardPagination/DashboardPagination";
+import ListTable2 from "../../components/AdminDashboard/ListTable2/ListTable2";
+const OrderInvoice = () => {
+  const person = [
+    {
+      td1: "1",
+      td2: "3746",
+      td3: "762",
+      td4: "70",
+      td5: "34937974",
+      td6: "active",
+    },
+    {
+      td1: "1",
+      td2: "3746",
+      td3: "762",
+      td4: "70",
+      td5: "34937974",
+      td6: "active",
+    },
+    {
+      td1: "1",
+      td2: "3746",
+      td3: "762",
+      td4: "70",
+      td5: "34937974",
+      td6: "active",
+    },
+  ];
+  return (
+    <div>
+      <DashboardBars>
+        <section id="show-merchants">
+          <h1 className="title mb-4">Customers</h1>
+          <div className="container">
+            <div className="row px-3  d-flex justify-content-between mb-5">
+              <div className=" mb-3 ">
+                <h1>Customer A</h1> 
+                <div>
+                  Singapor Address <br />
+                  Ph : 030303030
+                </div>
+               
+              </div>
+              <div className=" d-flex flex-column justify-content-cneter text-center ">
+                    <h4 className="mb-3">Invoice</h4>
+                    <div>
+                    <button className="btn text-white  py-1 px-5" style={{backgroundColor : "#43425d"}}>Print</button>
+
+                    </div>
+              </div>
+            </div>
+            <hr className="mb-3" />
+            <div className="row mb-3 px-3 d-flex justify-content-between">
+                <div className="">
+                  
+                  <strong>
+                   Invoice To</strong>
+                   <br />
+                   
+                       Singapore Adress <br />
+                       Postal Code,12345
+                   
+                </div>
+                <div className=" ">
+                    <strong>Order Date: </strong> Nov 15,2021 <br />
+                    <strong>Order Status: </strong> <span class="badge badge-pill badge-success">Pending</span> <br />
+                    <strong>Order Pending: </strong> #12345 <br />
+                </div>
+            </div>
+            <div className="row  mb-3">
+              <div className="col">
+                <ListTable2
+                  cols={[
+                    "Order ID",
+                    "Transaction Date",
+                    "Item",
+                    "Pending Orders",
+                    "Amount",
+                    "Status",
+                  ]}
+                  data={person}
+                  buttondis={true}
+                />
+              </div>
+            </div>
+            
+            <div className=" d-flex justify-content-end text-right p-4  ">
+                <div>
+                <strong>Sub Total: </strong> Nov 15,2021 <br />
+                    <strong>Discount: </strong> Pending <br />
+                    <strong className="">  Vat: </strong> #12345 <br />
+
+                    <h4 className="mt-3"><strong>SGD 2436</strong></h4>
+                    <button className="btn btn-warning py-1 px-4">Submit</button>
+                </div>
+                   
+                </div>
+
+            
+            {/* <DashboardPagination /> */}
+          </div>
+        </section>
+      </DashboardBars>
+    </div>
+  );
+};
+
+export default OrderInvoice;
