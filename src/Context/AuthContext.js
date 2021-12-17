6959210a43d2fcaef1fa407c928f23e79db7b79b
awaisshah228 <m.awaisshah228@gmail.com>
2021-12-17 16:44:39 +0500

    refresh token bug resolved

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -9,15 +9,16 @@ import {
   from,
 } from "@apollo/client";
 import axios from "axios";
+  axios.defaults.withCredentials = true;
+
 
 const AuthContext = createContext();
 export function AuthContextProvider(props) {
   const [token, settoken] = useState("");
-  axios.defaults.withCredentials = true;
 
-  async function setToken(tok) {
+  async function setToken(toks) {
     localStorage.setItem("firstLogin", true);
-    settoken(tok);
+    settoken(toks);
   }
 
   const httpLink = new HttpLink({
@@ -27,27 +28,32 @@ export function AuthContextProvider(props) {
 
   const authMiddleware = new ApolloLink((operation, forward) => {
     // add the authorization to the headers
-    operation.setContext(async ({ headers = {} }) => {
-      // const firstLogin =localStorage.getItem("firstLogin")
-      const firstLogin = localStorage.getItem("firstLogin");
-      if (firstLogin) {
-        const tok = await axios.post("http://localhost:4000/graphql", {
-          query: `
-          mutation Mutation {
-           generateAccessToken {
-           token  
-           }
-         }
-          `,
-        });
+    operation.setContext(async({ headers = {} }) => {
 
-        settoken(tok.data.data.generateAccessToken.token);
+      const firsLogin=localStorage.getItem('firstLogin')
+      if(firsLogin){
+             const tok = await axios.post("http://localhost:4000/graphql", {
+              query: `
+              mutation Mutation {
+               generateAccessToken {
+               token  
+               }
+             }
+              `,
+            },{
+              headers:{
+                authorization : token
+              }
+            })
+            settoken(tok.data.data.generateAccessToken.token)
+            
       }
+        
 
-      return {
+         return {
         headers: {
           ...headers,
-          authorization: token || null,
+          authorization: token||null ,
         },
       };
     });
@@ -56,15 +62,11 @@ export function AuthContextProvider(props) {
   });
 
   const client = new ApolloClient({
-    // uri: 'http://localhost:4000/graphql',
-    // credentials: 'include',
     cache: new InMemoryCache(),
     link: from([
       authMiddleware,
-      // activityMiddleware,
       httpLink,
     ]),
-    // link: concat(authMiddleware, httpLink),
   });
 
   return (
